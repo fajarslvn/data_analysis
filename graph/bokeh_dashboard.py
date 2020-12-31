6d40f4a4cbeeb94ded12a5d9d816d4f37140a4de
fajarslvn <jarphish@gmail.com>
2020-12-31 03:00:12 +0000

    fixed

--- graph/bokeh_dashboard.py
+++ graph/bokeh_dashboard.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-#%%
 import pandas as pd
 import numpy as np
 from bokeh.palettes import brewer
@@ -9,12 +8,12 @@ from bokeh.plotting import figure, output_file, show
 x = [2,4,6,8,10]
 y = [1,3,5,7,9]
 
-output_file('graph.html')
+output_file('graph.html', title='Simple Line')
 
 p = figure(
-    title = 'Simple Graph',
-    x_axis_label = 'X Axis',
-    y_axis_label = 'Y Axis'
+  title = 'Simple Graph',
+  x_axis_label = 'X Axis',
+  y_axis_label = 'Y Axis'
 )
 
 p.line(x, y, legend_label='line', line_width=2)
